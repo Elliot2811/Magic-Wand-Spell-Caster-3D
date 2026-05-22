0fb1644118fac47024ee2bf5c5e9b601c54d92f9
Elliot2811 <cylelliot@gmail.com>
2026-05-22 16:02:55 +0800

    Hot fix 1 after messy merge
    Remove scripts that did not want to not be merged into main

--- Assets/Scripts/Pvp scripts/scriptableObjectSpells.cs
+++ Assets/Scripts/Pvp scripts/scriptableObjectSpells.cs
@@ -1,12 +1,7 @@
 using UnityEngine;
 
-<<<<<<<< HEAD:Assets/Scripts/Pvp scripts/ScriptableObjectSpells.cs
 [CreateAssetMenu(fileName = "Basic Spell", menuName = "Spells/Basic/BasicSpell")]
 public class ScriptableObjectSpells : ScriptableObject
-========
-[CreateAssetMenu(fileName = "Basic Spell", menuName = "Spells/BasicSpell")]
-public class scriptableObjectSpells : ScriptableObject
->>>>>>>> projectile-script:Assets/Scripts/Pvp scripts/scriptableObjectSpells.cs
 {
     [Header("Spell settings")]
     public string spellName = "Input Spell Name";
